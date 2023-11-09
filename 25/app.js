33e66adc8c4cf4b07b31bdf4abd3648614eb2a2b
Suga-Motic <146709125+Suga-Motic@users.noreply.github.com>
2023-11-09 20:30:15 +0300

    Add files via upload

---
+++ 25/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/products/:productId', function (
+    request,
+    response
+) {
+    response.send(
+        'productId: ' + request.params['productId']
+    );
+});
+
+
+app.listen(3000);
